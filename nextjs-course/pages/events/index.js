cd4661da03199f183dfa1eba31b67147bf98bb03
giorgivashakmadzee-web <giorgivashakmadzee@gmail.com>
2026-08-18 21:38:38 +0400

    got a little demo goint by folowing the tutorial from the nextjs course. I have created a new folder called nextjs-course and added the pages and styles folders with the necessary files. The _app.js file is used to initialize pages, and the events folder contains dynamic routes for event details. The index.js file serves as the homepage for the application.

---
+++ nextjs-course/pages/events/index.js
@@ -0,0 +1,11 @@
+function ALLEventsPage() {
+    return(
+        <div>
+            <h1>ALLEventsPage</h1>
+        </div>
+    )
+};
+
+
+
+export default ALLEventsPage
---
+++ nextjs-course/pages/index.js
@@ -0,0 +1,6 @@
+function HomePage() {
+  return <div>homepage</div>
+}  
+
+
+export default HomePage
